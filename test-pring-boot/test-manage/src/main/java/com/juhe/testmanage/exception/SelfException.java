b8f19ce1cb819762ea0d484bb930a4a161b57a29
521xuxiao <37203926+521xuxiao@users.noreply.github.com>
2021-02-19 15:44:54 +0800

    Update SelfException.java

--- test-pring-boot/test-manage/src/main/java/com/juhe/testmanage/exception/SelfException.java
+++ test-pring-boot/test-manage/src/main/java/com/juhe/testmanage/exception/SelfException.java
@@ -1,1 +1,24 @@
+package com.erp.myerp.exception;
 
+import java.io.Serializable;
+
+public class SelfException extends RuntimeException{
+    public SelfException() {
+    }
+
+    public SelfException(String message) {
+        super(message);
+    }
+
+    public SelfException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SelfException(Throwable cause) {
+        super(cause);
+    }
+
+    public SelfException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
